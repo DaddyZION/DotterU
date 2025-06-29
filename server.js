d143a646433024a46afdfe55f71ad9bab1d58dc2
DaddyZION <kingzionsky@gmail.com>
2025-06-29 21:07:59 +0100

    modify

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
 const users = {}; // username: { x, y, color, ws, ip }
-const DOT_SIZE = 18;
+const DOT_SIZE = 54;
 const OVERLAY_WIDTH = 1920;
 const OVERLAY_HEIGHT = 1080;
 const ipUserCounts = {}; // { ip: count }
