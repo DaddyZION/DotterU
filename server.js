badb85d48ea196555afb1db983f88967f6986510
DaddyZION <kingzionsky@gmail.com>
2025-07-02 01:41:58 +0100

    j

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const DOT_SIZE = 48;
 const OVERLAY_WIDTH = 1920;
 const OVERLAY_HEIGHT = 1080;
 const ipUserCounts = {}; // { ip: count }
-const MAX_USERS_PER_IP = 2;
+const MAX_USERS_PER_IP = 50;
 
 // List of sprite filenames
 const SPRITES = [
